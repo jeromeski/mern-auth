31cd61c6c671c5c6c20959c7c7f7b89323ba0715
jeromeg-fullstack <jeromegee@gmail.com>
2020-08-08 17:59:12 +0800

    middlewares, connected mongodb, created controllers & routes

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB Connected'))
+  .catch(err => console.log(`DB Connection Error: ${err}`));
+
+const authRoutes = require('./routes/auth');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// route middlewares
+app.use('/api', authRoutes);
+
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
   console.log(`
   ==============================
