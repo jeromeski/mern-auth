5c6dabf48234c6091900b03a47d6b2cf72ecd140
jeromeg-fullstack <jeromegee@gmail.com>
2020-08-09 21:38:15 +0800

    Account Activation

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,6 +43,41 @@ exports.signup = (req, res) => {
   });
 };
 
+exports.accountActivation = (req, res) => {
+  const { token } = req.body;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function(
+      err,
+      decoded
+    ) {
+      if (err) {
+        console.log('JWT VERIFY IN ACTIVATION ERROR', err);
+        return res.status(401).json({
+          error: 'Expired link. Signup again'
+        });
+      }
+      const { name, email, password } = jwt.decode(token);
+      const user = new User({ name, email, password });
+      user.save((err, user) => {
+        if (err) {
+          console.log('SAVE USER IN ACCOUNT ACTIVATION ERROR', err);
+          return res.status(401).json({
+            error: 'Error saving user in database. Try signup again'
+          });
+        }
+        return res.json({
+          message: 'Signup success, Please signin.'
+        });
+      });
+    });
+  } else {
+    return res.json({
+      message: 'Signup success, Please signin.'
+    });
+  }
+};
+
 /*
 exports.signup = (req, res) => {
   console.log('Request Body :', req.body);
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../controllers/auth');
+const { signup, accountActivation } = require('../controllers/auth');
 
 //import validators
 const { userSignupValidator } = require('../validators/auth');
 const { runValidation } = require('../validators');
 
 router.post('/signup', userSignupValidator, runValidation, signup);
+router.post('/activation', accountActivation);
 
 module.exports = router;
