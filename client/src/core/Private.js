e5909506c44db8c33fd62680086a6e4afdc30838
jeromeg-fullstack <jeromegee@gmail.com>
2020-08-15 16:43:13 +0800

    Profile update page setup

--- client/src/core/Private.js
+++ client/src/core/Private.js
@@ -1,10 +1,105 @@
-import React from 'react';
-import Layout from './Layout';
+import React, { useState } from 'react';
+import Layout from '../core/Layout';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+// import { isAuth } from '../auth/helpers';
 
 const Private = () => {
+  const [values, setValues] = useState({
+    role: '',
+    name: '',
+    email: '',
+    password: '',
+    buttonText: 'Submit'
+  });
+
+  const { role, name, email, password, buttonText } = values;
+
+  const handleChange = name => event =>
+    setValues({ ...values, [name]: event.target.value });
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: 'Submitting' });
+    axios({
+      method: 'POST',
+      url: '/api/signup',
+      data: { name, email, password }
+    })
+      .then(response => {
+        console.log('SIGNUP SUCCESS', response);
+        setValues({
+          ...values,
+          role: '',
+          name: '',
+          email: '',
+          password: '',
+          buttonText: 'Submitted'
+        });
+        toast.success(response.data.message);
+      })
+      .catch(error => {
+        console.log('SIGNUP ERROR', error.response.data);
+        setValues({ ...values, buttonText: 'Submit' });
+        toast.error(error.response.data.error);
+      });
+  };
+
+  const updateForm = () => (
+    <form onSubmit={clickSubmit}>
+      <div className='form-group'>
+        <label className='text-muted'>Role</label>
+        <input type='text' value={role} readOnly className='form-control' />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Name</label>
+        <input
+          type='text'
+          value={name}
+          onChange={handleChange('name')}
+          className='form-control'
+        />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input type='email' value={email} readOnly className='form-control' />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          type='password'
+          value={password}
+          onChange={handleChange('password')}
+          className='form-control'
+        />
+      </div>
+      <div>
+        <button className='btn btn-primary' onClick={clickSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
   return (
     <Layout>
-      <h1>Private Page</h1>
+      <div className='col-md-6 offset-md-3'>
+        <ToastContainer
+          position='bottom-right'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+
+        <p className='pt-5 mb-0 lead text-center'>PRIVATE</p>
+        <h1 className='text-center'>Profile Update</h1>
+        {updateForm()}
+      </div>
     </Layout>
   );
 };
